dd19fcbb1ff1f834c85e9754359d2094b80adf9f
qinglong-kooboo <qinglong@kooboo.cn>
2019-03-28 13:50:04 +0800

    change pre-style-render

--- package/swiper/swiper.vue
+++ package/swiper/swiper.vue
@@ -12,8 +12,12 @@
     <div class="dots" >
       <span class="dot" v-for="(item, index) in dots" :key="index" :class="{active: nowIndex === index }" @click="goto(index)"></span>
     </div>
-    <span class="iconfont arrow-left" @click="goByArrow(prevIndex)">&#xe504;</span>
-    <span class="iconfont arrow-right" @click="goByArrow(nextIndex)">&#xe603;</span>
+    <div class="arrow-left-wrapper" @click="goByArrow(prevIndex)">
+      <span class="iconfont arrow-left">&#xe504;</span>
+    </div>
+    <div class="arrow-right-wrapper" @click="goByArrow(nextIndex)">
+      <span class="iconfont arrow-right">&#xe603;</span>
+    </div>
   </div>
 </template>
 
@@ -77,73 +81,93 @@ export default {
 }
 </script>
 
-<style scoped lang="stylus" rel="stylesheet/stylus">
-  .slider
-    position: relative
-    overflow: hidden
-    margin: 0
-    padding: 0
-    .slider-wrapper
-      width: 100%
-      height: 100%
-      list-style: none
-      overflow: hidden
-      margin: 0
-      padding: 0
-      .slider-item
-        position: absolute
-        width: 100%
-        height: 100%
-        overflow: hidden
-        img
-          width: 100%
-          height: 100%
-        &.move-enter-active
-          transition: all 0.5s ease
-          transform: translateX(0)
-        &.move-leave-active
-          transition: all 0.5s ease
-          transform: translateX(-100%)
-        &.move-enter
-          transform: translateX(100%)
-        &.move-leave
-          transform: translateX(0)
-        &.fade-enter-active, .fade-leave-active
-          transition: opacity 1s
-        &.fade-enter, .fade-leave-active
-          opacity: 0.5
-    .arrow-right, .arrow-left
-      position absolute
-      top 45%
-      font-size 1.5em
-      padding 3px
-      border-radius 50%
-      background-color #e6e6e6
-      cursor pointer
-      user-select none
-      opacity .3
-      &:hover
-        opacity 1
-    .arrow-left
-      left 10px
-    .arrow-right
-      right 10px
-    .dots
-      position: absolute
-      right: 0
-      left: 0
-      bottom: 20px
-      text-align: center
-      font-size: 0
-      .dot
-        display: inline-block
-        margin: 0 4px
-        width: 8px
-        height: 8px
-        border-radius: 50%
-        background: rgba(255,255,255,0.6)
-        &.active
-          width: 20px
-          border-radius: 5px
-          background: rgba(255,255,255,0.9)
+<style scoped lang="scss">
+  .slider {
+    position: relative;
+    overflow: hidden;
+    margin: 0;
+    padding: 0;
+    .slider-wrapper {
+      width: 100%;
+      height: 100%;
+      list-style: none;
+      overflow: hidden;
+      margin: 0;
+      padding: 0;
+      .slider-item {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        img {
+          width: 100%;
+          height: 100%;
+        }
+        &.move-enter-active {
+          transition: all 0.5s ease;
+          transform: translateX(0);
+        }
+        &.move-leave-active {
+          transition: all 0.5s ease;
+          transform: translateX(-100%);
+        }
+        &.move-enter {
+          transform: translateX(100%);
+        }
+        &.move-leave {
+          transform: translateX(0);
+        }
+        &.fade-enter-active, .fade-leave-active {
+          transition: opacity 1s;
+        }
+        &.fade-enter, .fade-leave-active {
+          opacity: 0.5;
+        }
+      }
+    }
+    .arrow-left-wrapper, .arrow-right-wrapper {
+      position: absolute;
+      top: 45%;
+      display: flex;
+      justify-content: center;
+      width: 24px;
+      height: 24px;
+      line-height: 24px;
+      border-radius: 50%;
+      background-color: #e6e6e6;
+      cursor: pointer;
+      user-select: none;
+      opacity: .3;
+      &:hover {
+        opacity: 1;
+      }
+    }
+    .arrow-left-wrapper {
+      left: 10px;
+    }
+    .arrow-right-wrapper {
+      right: 10px;
+    }
+    .dots {
+      position: absolute;
+      right: 0;
+      left: 0;
+      bottom: 20px;
+      text-align: center;
+      font-size: 0;
+      .dot {
+        display: inline-block;
+        margin: 0 4px;
+        width: 8px;
+        height: 8px;
+        border-radius: 50%;
+        background: rgba(255,255,255,0.6);
+        &.active {
+          width: 20px;
+          border-radius: 5px;
+          background: rgba(255,255,255,0.9);
+        }
+      }
+    }
+  }
 </style>
